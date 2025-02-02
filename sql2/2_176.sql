df95601ae399ba115d6d8e18fd46647a67bcc27d
Kelly liu <shu@liu.net>
2025-02-02 12:06:30 -0800

    sql coalesce()

---
+++ sql2/2_176.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+-- Method 1
+SELECT COALESCE(
+    (SELECT MAX(salary) FROM Employee WHERE salary < (SELECT MAX(salary) FROM Employee)),
+    NULL
+) AS SecondHighestSalary;
+
+-- Method 2
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
