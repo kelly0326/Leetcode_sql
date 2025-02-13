2d3597a5cb931e92351d73a979572f98a148bd44
Kelly liu <shu@liu.net>
2025-02-13 10:27:11 -0800

    Using both with clause and subqueries to solve one sql problem

--- sql2/2_1581.sql
+++ sql2/2_1581.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+
+# Method 1: with clause
 with cte as
 (select * from transactions t
 where amount is not null)
@@ -6,4 +8,10 @@ where amount is not null)
 select customer_id, count(customer_id) as count_no_trans
 from visits
 where visit_id not in (select visit_id from cte)
+group by customer_id;
+
+# Method 2: subqueries
+select customer_id, count(customer_id) as count_no_trans
+from visits
+where visit_id not in (select visit_id from transactions)
 group by customer_id;
