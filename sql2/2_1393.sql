5e5ab2e4510b71ed35c154dbba416c36903647c1
Kelly liu <shu@liu.net>
2025-03-09 12:17:32 -0700

    added Pandas version solution from 1393 sql

--- sql2/2_1393.sql
+++ sql2/2_1393.sql
@@ -9,3 +9,14 @@ from stocks
 group by stock_name;
 
 # Pandas solution
+import pandas as pd
+
+def capital_gainloss(stocks: pd.DataFrame) -> pd.DataFrame:
+    temp = stocks[['stock_name', 'operation', 'price']]
+    temp['operation'] = temp['operation'].map({"Buy": -1, "Sell": 1}) # same as sql case when
+    temp['capital_gain_loss'] = temp['price']*temp['operation']
+    temp2 = temp.groupby('stock_name').sum('capital_gain_loss')
+    temp2 = temp2[['capital_gain_loss']]
+    temp2 = temp2.reset_index()
+    print(temp2)
+    return temp2
