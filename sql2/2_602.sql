3e14cee061e475e7c9f8629f7483dacaea2fd807
Kelly liu <shu@liu.net>
2025-03-22 09:59:56 -0700

    sql union all & with clause

---
+++ sql2/2_602.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+with table1 as
+(select requester_id as id, count(accepter_id) as num
+from requestaccepted
+group by requester_id),
+
+table2 as
+(select accepter_id as id, count(requester_id) as num
+from requestaccepted
+group by accepter_id),
+
+table3 as
+(select *
+from table1
+UNION ALL 
+select *
+from table2)
+
+select id, sum(num) as num
+from table3
+group by id
+order by num desc
+limit 1;
