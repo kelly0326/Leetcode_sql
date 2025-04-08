141778c9b2886dff78cf9200d06b6a43f7b73ed6
Kelly liu <shu@liu.net>
2025-04-08 10:46:34 -0700

    sql ctes practice

--- sql2/2_3497.sql
+++ sql2/2_3497.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+
+# Method 1
 with base as
 (select *
 from UserActivity
@@ -18,5 +20,28 @@ group by user_id, activity_type)
 select user_id, 
 	   sum(trial_avg_duration) as trial_avg_duration, sum(paid_avg_duration) as paid_avg_duration
 from base1
+group by user_id
+order by user_id;
+
+
+# Method 2
+with base as
+(select user_id, activity_type, round(sum(activity_duration) / count(user_id), 2) as temp
+from useractivity
+where user_id IN 
+(SELECT distinct user_id
+from useractivity
+where activity_type = 'paid')
+group by user_id, activity_type)
+
+select user_id,
+       sum(case when activity_type = 'free_trial' then temp
+       else 0
+       end) as trial_avg_duration,
+
+       sum(case when activity_type = 'paid' then temp
+       else 0
+       end) as paid_avg_duration
+from base
 group by user_id
 order by user_id;
