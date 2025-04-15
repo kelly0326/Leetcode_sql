1eb5ec28ae1aab0967fc7a0b56c0ce11b464133c
Kelly liu <shu@liu.net>
2025-04-15 10:04:37 -0700

    sql update clause practice

--- sql1/1_627.sql
+++ sql1/1_627.sql
@@ -1,6 +1,15 @@
 # Write your MySQL query statement below
+-- Method 1
 update salary
 set sex = case
             when sex = 'm' then 'f'
             when sex = 'f' then 'm'
         end;
+
+-- Method 2
+update salary
+set sex =
+    case 
+    when sex = 'm' then 'f'
+    else 'm'
+    end;
