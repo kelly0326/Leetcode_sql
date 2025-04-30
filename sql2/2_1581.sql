52c92e1c6391c088b98942d28927a857fb1f9ce9
Kelly liu <shu@liu.net>
2025-04-30 08:03:23 -0700

    sql practice continued by Shu

--- sql2/2_1581.sql
+++ sql2/2_1581.sql
@@ -15,3 +15,11 @@ select customer_id, count(customer_id) as count_no_trans
 from visits
 where visit_id not in (select visit_id from transactions)
 group by customer_id;
+
+-- METHOD 3
+select customer_id, count(customer_id) as count_no_trans
+from visits v
+left join transactions t
+on v.visit_id = t.visit_id
+where t.visit_id is null
+group by customer_id;
--- sql2/2_1789.sql
+++ sql2/2_1789.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+-- Method 1
 with cte as
 (select employee_id, department_id
 from employee
@@ -12,3 +13,21 @@ from employee
 where primary_flag = 'Y')
 select * from cte
 order by employee_id;
+
+-- Method 2
+with cte as
+(select employee_id, department_id
+from employee
+group by employee_id
+having count(department_id) = 1),
+
+cte1 as
+(select employee_id, department_id
+from employee
+where primary_flag = 'Y')
+
+select *
+from cte
+union
+select *
+from cte1;
