fa16d04c45ecd4a77e77767728dbb0ca690ff219
Kelly liu <shu@liu.net>
2025-06-11 14:21:39 -0700

    Sql practice

--- sql1/1_1527.sql
+++ sql1/1_1527.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT patient_id, patient_name, conditions
 FROM Patients
-WHERE conditions LIKE 'DIAB1%'     -- starts with DIAB1
-OR conditions LIKE '% DIAB1%';  -- has DIAB1 after a space
+WHERE conditions LIKE 'DIAB1%'     -- starts with DIAB1 *
+OR conditions LIKE '% DIAB1%';  -- has DIAB1 after a space *
