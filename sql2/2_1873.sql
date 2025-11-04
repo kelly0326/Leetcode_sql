9d572132bd3eace933a3b1a54d21131d7322e58a
Kelly liu <shu@liu.net>
2025-11-04 09:57:07 -0800

    sql updated

--- sql2/2_1873.sql
+++ sql2/2_1873.sql
@@ -1,4 +1,4 @@
-# MySQL query statement updated
+# MySQL query statement
 select employee_id, 
     case when employee_id % 2 = 1 and name not like 'M%' then salary else 0 end as bonus
 from employees
